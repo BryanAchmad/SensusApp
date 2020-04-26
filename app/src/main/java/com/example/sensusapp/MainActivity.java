27a5c1eefe2e91b229eda30702884afa869f1155
Bryan <bryanachmad08@gmail.com>
2020-04-26 23:07:37 +0700

    a

--- app/src/main/java/com/example/sensusapp/MainActivity.java
+++ app/src/main/java/com/example/sensusapp/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.sensusapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppCompatButton btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = (AppCompatButton) findViewById(R.id.btnlogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
